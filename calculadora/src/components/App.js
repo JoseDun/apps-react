46e1b427e208222152cd1e91f4cabc21a48410ca
JoseDun <josedunsh@gmail.com>
2022-05-24 18:19:43 -0400

    falta agregar mathjs

--- calculadora/src/components/App.js
+++ calculadora/src/components/App.js
@@ -1,9 +1,51 @@
 import "./App.css";
+import logo from "../images/index";
+import Boton from "./boton/Boton";
+import Pantalla from "./pantalla/Pantalla";
+import BotonClear from "./botonClear/BotonClear";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const agregarInput = (val) => {
+    setInput(input + val);
+  };
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <div className="freecodecamp-logo-contenedor">
+        <img src={logo} alt="logo" className="freecodecamp-logo" />
+      </div>
+
+      <div className="contenedor-calculadora">
+        <Pantalla input={input} />
+        <div className="fila">
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic={agregarInput}> = </Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
+        </div>
+        <div className="fila">
+          <BotonClear manejarClear={()=> setInput('')}>Clear</BotonClear>
+        </div>
+      </div>
     </div>
   );
 }
