f94af93db69b0c92ca4fa8f4392890f267b1fb52
JoseDun <josedunsh@gmail.com>
2022-05-31 16:20:40 -0400

    lista app tareas

--- app-de-tareas/src/components/App.jsx
+++ app-de-tareas/src/components/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import ListaDeTareas from "./ListaDeTareas/ListaDeTareas";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <div className="tareas-lista-principal">
+        <h1>Mis tareas</h1>
+        <ListaDeTareas />
+      </div>
     </div>
   );
 }
---
+++ app-de-tareas/src/components/ListaDeTareas/ListaDeTareas.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Formulario from "../Formulario/Formulario";
+import "./ListaDeTareas.css";
+import Tarea from "../Tarea/Tarea";
+
+function ListaDeTareas() {
+  const [tareas, setTareas] = useState([]);
+
+  const agregarTarea = (tarea) => {
+    if (tarea.texto.trim()) {
+      tarea.texto = tarea.texto.trim();
+      const tareasActualizadas = [tarea, ...tareas];
+      setTareas(tareasActualizadas);
+    }
+  };
+
+  const completarTarea = (id) => {
+    const tareasActualizadas = tareas.map((tarea) => {
+      if (tarea.id === id) {
+        tarea.completada = !tarea.completada;
+      }
+      return tarea;
+    });
+    setTareas(tareasActualizadas);
+  };
+
+  const eliminarTarea = (id) => {
+    const tareasActualizadas = tareas.filter((tarea) => tarea.completada === false || tarea.id !== id);
+    setTareas(tareasActualizadas);
+  };
+
+  return (
+    <>
+      <Formulario onSubmit={agregarTarea} />
+      <div className="tareas-lista-contenedor">
+        {tareas.map((tarea) => (
+          <Tarea
+            key={tarea.id}
+            id={tarea.id}
+            texto={tarea.texto}
+            completada={tarea.completada}
+            eliminarTarea={eliminarTarea}
+            completarTarea={completarTarea}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default ListaDeTareas;
